a998512657de0def62dd3c28ddb652afd124941b
jaeeewon <hello.pleizz@gmail.com>
2026-08-17 20:49:05 +0900

    chore: set port and host relateds

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/ws': { target: 'ws://localhost:8787', ws: true },
       '/api': { target: 'http://localhost:8787', changeOrigin: true },
